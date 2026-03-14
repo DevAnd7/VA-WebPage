070a9b9e43ff0070cafc2a2b8e53c1bb22de7542
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-14 20:47:18 +0000

    creacion del navbar

---
+++ VA-WP/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+                        Virgilio Arrieta
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
