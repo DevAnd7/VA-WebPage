5f2f54999b112ea719963481a789d71f9b3b0e5f
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-14 22:35:29 +0000

    creacion del footer

--- VA-WP/src/App.jsx
+++ VA-WP/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <div className="container">
       <Navbar />
+      <Footer />
     </div>
   )
 }
---
+++ VA-WP/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="text-white text-center py-3 mt-5">
+      <div className="container">
+        <small>© 2026 Virgilio Arrieta — Todos los derechos reservados</small>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
