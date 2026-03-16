4200c8c9f0e7bb68e48ff04557016a63c4f23321
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-16 07:44:42 +0000

    Creacion del banner con 300px y overlay

--- VA-WP/src/Components/Banner.jsx
+++ VA-WP/src/Components/Banner.jsx
@@ -1,17 +1,15 @@
+import "./Banner.css"; //importacion de los estilos del banner
+
 function Banner() {
     return (
         <div className="card bg-dark text-white mb-2">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="/src/public/VA taller.webp" className="img-fluid rounded-start" alt="..." />
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
+            <div className="card text-bg-dark">
+                <img src="VA taller.webp" className="card-img-banner" alt=""/>
+                    <div className="card-img-overlay">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                        <p className="card-text"><small>Last updated 3 mins ago</small></p>
                     </div>
-                </div>
             </div>
         </div>
     );
