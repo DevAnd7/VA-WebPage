3fc599d448e2f492d4aa70a6f9a697c10b96f98b
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-16 12:41:15 +0000

    justificacion texto banner

--- VA-WP/src/Components/Banner.jsx
+++ VA-WP/src/Components/Banner.jsx
@@ -3,13 +3,13 @@ import "./Banner.css"; //importacion de los estilos del banner
 function Banner() {
     return (
         <div className="card bg-dark text-white mb-2">
-            <div className="card text-bg-dark">
-                <img src="VA taller.webp" className="card-img-banner" alt=""/>
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small>Last updated 3 mins ago</small></p>
-                    </div>
+            <img src="VA taller.webp" className="card-img-banner" alt="" />
+            <div className="card-img-overlay d-flex justify-content-center flex-column text-start">
+                <h5 className="card-title fs-2">Aquí la creatividad, no tiene límites.</h5>
+                <p className="card-text fs-3">
+                    Artista polifacético que abarca la pintura, el muralismo, la escultura, La música, la composición, la escritura y la producción.
+                </p>
+                <p className="card-text"><small>Last updated 3 mins ago</small></p>
             </div>
         </div>
     );
