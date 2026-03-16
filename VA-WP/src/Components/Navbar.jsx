cb6741928046e831ebdfc7438a1f5132f0c2dfec
Andri Arrieta <andriarrieta7@gmail.com>
2026-03-16 12:27:56 +0000

    modificacion del navbar

--- VA-WP/src/Components/Navbar.jsx
+++ VA-WP/src/Components/Navbar.jsx
@@ -1,11 +1,29 @@
 function Navbar() {
     return (
-        <nav className="navbar navbar-dark bg-dark mb-2">
-            <div className="container d-flex">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+            <div className="container">
                 <a className="navbar-brand d-flex align-items-center gap-2" href="#">
                     <i className="bi bi-palette"></i>
-                    <span className="fs-1">Virgilio Arrieta</span>
+                    <span className="fs-3">Virgilio Arrieta</span>
                 </a>
+
+                <button 
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                        <a className="nav-link active" href="#">Inicio</a>
+                        <a className="nav-link" href="#">Pintor</a>
+                        <a className="nav-link" href="#">Compositor</a>
+                        <a className="nav-link" href="#">Escritor</a>
+                        <a className="nav-link" href="#">Biografía</a>
+                    </div>
+                </div>
             </div>
         </nav>
     );
--- VA-WP/src/main.jsx
+++ VA-WP/src/main.jsx
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
 import "bootstrap-icons/font/bootstrap-icons.css"; // Importa los estilos de Bootstrap Icons
-import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'//importamos el JS de Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import './index.css'//importamos los estilos personalizados
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
