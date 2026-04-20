23646c72f24830aed892818b917c693a73e41fe2
Andri Arrieta <andriarrieta7@gmail.com>
2026-04-20 09:51:01 +0000

    Musica Discografia

--- src/Pages/Musica.jsx
+++ src/Pages/Musica.jsx
@@ -3,6 +3,13 @@ import Cancion from "../Components/Cancion";
 const Musica = () => {
     return (
         <div className="container">
+
+            <h2>Música - Discografía</h2>
+
+            <p>Bienvenidos a la sección de Discografía y Letras, donde podrás explorar la trayectoria musical de Virgilio Arrieta a través de sus álbumes y canciones. Aquí encontrarás cada una de sus producciones, desde sus primeros lanzamientos hasta sus más recientes éxitos, así como las letras que dan vida a su música.</p>
+
+            <strong className="d-flex gap-2 p-2"><i className="bi bi-chat-text"></i>Cada canción cuenta una historia, un reflejo de sus experiencias, emociones y el estilo único que lo caracteriza. Te invitamos a sumergirte en su mundo sonoro y a descubrir el significado detrás de cada verso.</strong>
+
             <div className="seccion row">
 
                 <div className="col-lg-4">
